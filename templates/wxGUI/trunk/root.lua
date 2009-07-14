d74d7fa92dc7e1a71c1fba445fa7670b61cacf93
rpusztai <rpusztai@735a8d64-39d1-11de-9b3e-8f53452658d7>
2009-07-14 16:46:53 +0000

    Removed all the links and include paths because they are not generic.

--- templates/wxGUI/trunk/root.lua
+++ templates/wxGUI/trunk/root.lua
@@ -5,29 +5,17 @@ package.name								= project.name
 -- UNIT TESTING SETTING --------------------------------------------------------
 --
 package.kind								= "winexe"
-package.testdirs							= { "tests" }
-package.testexcludes						= { package.name..".cpp" }
 
 package.files								= {
 												matchfiles( "*.cpp", "*.h", "*.fbp" )
 											  }
 
 package.includepaths						= {
-												"lua",
-												"gtx",
-												"devicecomm",
-												"loki",
-												"boost_utils",
-												"wxtools"
+
 											  }
 
 package.links								= {
-												"DeviceComm",
-												"LuaLib",
-												"gtxComm",
-												"gtxAdvanced",
-												"gtxCore",
-												"Loki"
+
 											  }
 
 -- PACKAGE SETUP --------------------------------------------------------------
