a783b11c3ef6b757d78707dcb4f750ef05c03df0
bennettcleveland <bennettcleveland@735a8d64-39d1-11de-9b3e-8f53452658d7>
2011-04-11 18:56:51 +0000

    Preparation for adding company-specific links and include directories to the premake file.

--- templates/console/trunk/root4.lua
+++ templates/console/trunk/root4.lua
@@ -10,13 +10,17 @@
 -- GENERAL SETUP -------------------------------------------------------------
 --
 project	"$(ProjectName)"
-
--- UNIT TESTING SETTING --------------------------------------------------------
---
 kind	"ConsoleApp"
 files	{ "*.cpp", "*.h", "*.lua", "*.fbp" }
+
+$(Links)
+
+$(IncludeDirs)
 
 -- PROJECT SETUP --------------------------------------------------------------
 --
 --boost.Configure( { "system", "regex", "thread", "wserialization", "serialization", "filesystem" } )
 Configure()
+
+-- UNIT TESTING SETTING --------------------------------------------------------
+--
